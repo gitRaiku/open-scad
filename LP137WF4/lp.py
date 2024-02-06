c6c38d7bffa5f202c9ff85489949b41fd1e39391
gitRaiku <sebi.pintilie@gmail.com>
2024-02-06 20:05:39 +0200

    Nu pot si nici nu vreau sa traiesc fara sa beau

--- LP137WF4/lp.py
+++ LP137WF4/lp.py
@@ -2,7 +2,7 @@ from kms import *
 
 set_global_fn(20)
 
-screenSize = [400, 231, 4]
+screenSize = [400, 231, 4, 10] # x y up down
 activeSize = [382, 215, 4]
 
 bracketSize = [15, 8.56, 0.3]
@@ -41,18 +41,38 @@ screenBox += cBracketBoxR.translate(-bracketPos[0], -bracketPos[1], bracketPos[2
 
 coverYMargins = [10 + bracketSize[1], 
                  10 + pcbSize[1]]
-coverMargin = [10, coverYMargins[0] + coverYMargins[1], 0]
+coverMargin = [10, coverYMargins[0] + coverYMargins[1], 4]
 coverSize = [screenSize[0] + coverMargin[0] * 2, 
              screenSize[1] + coverMargin[1], 
-             screenSize[2] + 2, 10]
+             screenSize[2] + 2, screenSize[3] + coverMargin[2]]
+print(coverSize)
 coverPos = [0, (coverYMargins[0] - coverYMargins[1]) / 2, 0]
 coverArea = center(sq(coverSize[0], coverSize[1]), coverSize, screenSize).translate(coverPos[0], coverPos[1], coverPos[2])
 
 screenFullBox = screenArea.linear_extrude(screenSize[2])
 coverBoxTop = coverArea.linear_extrude(coverSize[2]) - screenFullBox - activeBox
-coverBoxBot = coverArea.linear_extrude(coverSize[3]).down(coverSize[3]) - screenArea.linear_extrude(coverSize[3] - 1).down(coverSize[3] - 1)
+coverBoxBot = coverArea.linear_extrude(coverSize[3]).down(coverSize[3]) - screenArea.linear_extrude(coverSize[3] - coverMargin[2] + 0.01).down(coverSize[3] - coverMargin[2])
 
-ccubS = [21, 28, 29]
-ccub = chc(ccubS, 45, 3, type=0b110)
-final = coverBoxBot 
+champferSize = [180, 180, 10]
+vesaSize = [150, 150, 4]
+champferDepth = 2
+champfer = center(chc(champferSize, 70, 5, type=0b110).color([0.7, 0.6, 0.8]), champferSize, screenSize).translate(coverPos).down(screenSize[3] + coverMargin[2] - champferDepth + champferSize[2])
+
+vesaArea = sq(vesaSize)
+hole = center(cr(4.3), [0, 0], vesaSize)
+vesaBox = center(vesaArea - \
+            hole.translate( 50,  50, 0) - \
+            hole.translate(-50,  50, 0) - \
+            hole.translate( 50, -50, 0) - \
+            hole.translate(-50, -50, 0), vesaSize, screenSize).linear_extrude(vesaSize[2]).color([0.3, 0.5, 0.1]).translate(coverPos).down(screenSize[3] + coverMargin[2] + champferDepth)
+
+vesaHoles = center(hole.translate( 50,  50, 0) + \
+                   hole.translate(-50,  50, 0) + \
+                   hole.translate( 50, -50, 0) + \
+                   hole.translate(-50, -50, 0), vesaSize, screenSize).linear_extrude(vesaSize[2]).color([0.3, 0.9, 0.1]).translate(coverPos).down(screenSize[3] + coverMargin[2] + champferDepth - vesaSize[2])
+
+coverBoxBot = (coverBoxBot - champfer + vesaBox) - vesaHoles
+
+# final = coverBoxBot + coverBoxTop
+final = coverBoxBot
 final.save_as_scad("lp.scad")
--- kms.py
+++ kms.py
@@ -1,6 +1,7 @@
 import solid2 as S
 import seaborn as sns
 import random
+from math import sqrt, tan
 
 def set_global_fn(fn):
     S.set_global_fn(fn)
@@ -8,14 +9,24 @@ def set_global_fn(fn):
 max_colours = 10
 cpalletes = sns.color_palette("husl", max_colours)
 random.seed(2)
+
+def a2r(x):
+    return x * 3.1415 / 180
+
+def ta(x):
+    return tan(a2r(x))
+
 def gcol():
     return cpalletes[random.randrange(0, max_colours)]
 
 def gc(x):
     return x.color(gcol())
 
-def sq(sx, sy):
-    return S.square(sx, sy, center=False).color(gcol())
+def sq(*args):
+    if len(args) == 2:
+        return S.square(args[0], args[1]).color(gcol())
+    else:
+        return S.square(args[0][0], args[0][1]).color(gcol())
 
 def cr(sx):
     return S.circle(sx).color(gcol())
@@ -82,22 +93,25 @@ def s(*args):
 
 def chc(size, angle, depth, type = 0b111):
     zc = S.cube(size[0], size[1], size[2])
-    rc = zc
+    rc = S.cube(size[0], size[1], size[2])
+
     if type & 0b001:
-        rc -= zc.rotate([0, 0, 45]).translate([0, size[1] - depth, 0]) + \
-              zc.rotate([0, 0, 135]).translate([depth, 0, 0]) + \
-              zc.rotate([0, 0, 225]).translate([size[0], depth, 0]) + \
-              zc.rotate([0, 0, 315]).translate([size[0] - depth, size[1], 0])
+        rc -= zc.rotate([0, 0,  90 - angle]).translate([0, size[1] - depth, 0]) + \
+              zc.rotate([0, 0,  90 + angle]).translate([depth * ta(angle), 0, 0]) + \
+              zc.rotate([0, 0, 270 - angle]).translate([size[0], depth, 0]) + \
+              zc.rotate([0, 0, 270 + angle]).translate([size[0] - depth * ta(angle), size[1], 0])
     if type & 0b010:
-        rc -= zc.rotate([0, 45, 0]).translate([size[0] - depth, 0, 0]) + \
-              zc.rotate([0, 135, 0]).translate([0, 0, depth]) + \
-              zc.rotate([0, 225, 0]).translate([depth, 0, size[2]]) + \
-              zc.rotate([0, 315, 0]).translate([size[0], 0, size[2] - depth])
+        angle = 90 - angle
+        rc -= zc.rotate([0,  90 - angle, 0]).translate([size[0] - depth, 0, 0]) + \
+              zc.rotate([0,  90 + angle, 0]).translate([0, 0, depth * ta(angle)]) + \
+              zc.rotate([0, 270 - angle, 0]).translate([depth, 0, size[2]]) + \
+              zc.rotate([0, 270 + angle, 0]).translate([size[0], 0, size[2] - depth * ta(angle)])
+        angle = 90 - angle
     if type & 0b100:
-        rc -= zc.rotate([45, 0, 0]).translate([0, size[1], size[2] - depth]) + \
-              zc.rotate([135, 0, 0]).translate([0, depth, size[2]]) + \
-              zc.rotate([225, 0, 0]).translate([0, 0, depth]) + \
-              zc.rotate([315, 0, 0]).translate([0, size[1] - depth, 0])
+        rc -= zc.rotate([ 90 - angle, 0, 0]).translate([0, 0, size[2] - depth]) + \
+              zc.rotate([ 90 + angle, 0, 0]).translate([0, depth * ta(angle), 0]) + \
+              zc.rotate([270 - angle, 0, 0]).translate([0, size[1], depth]) + \
+              zc.rotate([270 + angle, 0, 0]).translate([0, size[1] - depth * ta(angle), size[2]])
     return rc
 def s(*args):
     if len(args) == 3:
@@ -129,4 +143,56 @@ def rcb(c, *args):
 
     return c.translate(neg(half(scale))).rotate(ang).translate(half(scale))
 
+def hex(d):
+    return S.circle(d = d, _fn = 6)
+
+def drik(d):
+    return S.circle(d = d, _fn = 3)
+
+def schraube(m, h, counterSunk=False, actualHeight=0, topHeight=0, topDiameter=0, headType=0):
+    pitch = 0.0
+    bore = 0.0
+    if topHeight == 0:
+        topHeight = m
+    if topDiameter == 0:
+        topDiameter = m * 2
+    if actualHeight == 0:
+        actualHeight = h
+    if m == 3:
+        pitch = 0.5
+        bore = 3.2
+    elif m == 4:
+        pitch = 0.7
+        bore = 4.3
+
+    threadHeight = sqrt(3) / 2 * pitch
+    centerD = bore - threadHeight
+    angel = (h / pitch) * 120
+    thread = drik(m).linear_extrude(h, twist=angel)
+    centerBore = S.circle(d = centerD).linear_extrude(actualHeight)
+
+
+    top = S.cylinder(topHeight, d1 = centerD if counterSunk else topDiameter, d2 = topDiameter)
+
+    gauraDepth = 0.8
+    if headType == 0:
+        phillipsWidth = 0.14
+        phillipsLength = 0.8
+        top = top - \
+            S.square([topDiameter * phillipsWidth, topDiameter * phillipsLength], center=True).linear_extrude(gauraDepth + 1).up(topHeight - gauraDepth) - \
+            S.square([topDiameter * phillipsLength, topDiameter * phillipsWidth], center=True).linear_extrude(gauraDepth + 1).up(topHeight - gauraDepth) 
+    elif headType == 1:
+        phillipsWidth = 0.2
+        phillipsLength = 0.8
+        top = top - \
+            S.square([topDiameter * phillipsLength, topDiameter * phillipsWidth], center=True).linear_extrude(gauraDepth + 1).up(topHeight - gauraDepth)
+    elif headType == 2:
+        top = top - \
+            hex(4).linear_extrude(gauraDepth + 1).up(topHeight - gauraDepth)
+            # TODO: This 4 isn't actually what it should be
+
+    return thread + centerBore + top.up(actualHeight)
+
+    # centerThign = S.cylinder(h, m, m)
+    
 
